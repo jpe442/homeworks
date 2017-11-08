9c005ff62f6296af2b01fea050f21103800607c3
Josh <shamanseye@gmail.com>
2017-11-08 09:00:41 -0800

    W6D3 homework complete

---
+++ W6D3/ajax/js/index.js
@@ -0,0 +1,18 @@
+console.log("Hello from the JavaScript console!");
+
+// Your AJAX request here
+
+$.ajax({
+  type: 'GET',
+  url: 'http://api.openweathermap.org/data/2.5/weather' +
+  '?q=London,uk&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  success(data) {
+    console.log("The weather as requested!")
+    console.log(data);
+  },
+  error() {
+    console.error("There were errors.");
+  },
+});
+
+console.log("AJAX sent")
